4412e663311a7c8794507d24fa683163282e4392
Rajasekaran S <proffraja@gmail.com>
2021-05-31 21:56:28 +0530

    testing plugin"

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask,jsonify,request
 from flask.templating import render_template
-from main import *;
-from she import *;
+from main import *
+from she import *
+from flask_cors import CORS
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 def hello_world():
