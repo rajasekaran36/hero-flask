fe16867594eeffc80b8e5eaf96fad6698e93bf90
Rajasekaran S <proffraja@gmail.com>
2021-05-31 11:14:27 +0530

    fix

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def index():
-    return "HELLOOO"
+    return "HELLOOOO"
 
 if __name__ == "__main__":
     app.run()
